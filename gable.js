6e83b43c2f1260001ed7958288c482fb163bb937
Taylor Buley <taylor.buley@gmail.com>
2011-12-28 21:15:53 -0500

    /s/Namespace/Gable/

---
+++ gable.js
@@ -0,0 +1,607 @@
+/* Gable.js */
+/* Goal: 1) Transform semi-structured JS objects into CSV and the standard Google Data Table object format, making it usable w/the Google Chart API. 2) Use the ChartWrapper class to create standard chart types (http://code.google.com/apis/chart/interactive/docs/reference.html#chartwrapperobject) based on Google Data Tables. 
+*/
+var Gable = {};
+Gable.cache = Gable.cache || {};
+Gable.utils = Gable.utils || {};
+Gable.data = Gable.data || {};
+Gable.data.utils = Gable.data.utils || {};
+Gable.data.type = Gable.data.type || {};
+Gable.data.types = Gable.data.types || {};
+Gable.data.value = Gable.data.value || {};
+Gable.data.values = Gable.data.values || {};
+Gable.data.table = Gable.data.table || {};
+Gable.data.row = Gable.data.row || {};
+Gable.data.column = Gable.data.column || {};
+Gable.data.cell = Gable.data.cell || {};
+Gable.chart = Gable.chart || {};
+Gable.charts = Gable.charts || {};
+
+/* CONFIG */
+
+/* Data Types */
+
+Gable.data.types = {
+    'table': {
+        'transform': {
+            'raw': function() {},
+        },
+        'validate': function() {}
+    },
+    'input': {
+        'transform': {
+            'raw': function() {},
+        },
+        'validate': function() {}
+    },
+    'raw': {
+        'transform': {
+            'table': function() {},
+            'csv': function() {}
+        },
+        'validate': function() {}
+    },
+    'csv': {
+        'transform': {
+            'raw': function() {},
+        },
+        'validate': function() {}
+    }
+};
+
+Gable.data.types.raw.transform.table = function(obj) {
+    var newobj = obj;
+
+    
+    return newobj;
+};
+
+Gable.data.types.raw.transform.csv = function(obj) {
+
+};
+
+Gable.data.types.raw.transform.filter = function(obj) {
+    var newobj = (null !== obj && 'undefined' !== typeof obj) ? obj : {},
+        objlen = obj.length,
+        x, current, attr;
+    if (Gable.utils.isArray(obj)) {
+        //zero-indexed array
+        for (x = objlen; x !== 0; x -= 1) {
+            current = obj[x];
+            //forgive non arrays by coercing them into a single item list
+            if (Gable.utils.isArray(current)) {
+                newobj[x] = current;
+            } else {
+                newobj[x] = [current];
+            }
+        }
+    } else {
+        //unique id object
+        for (attr in obj) {
+            if (obj.hasOwnProperty(attr)) {
+                current = obj[attr];
+                if (Gable.utils.isArray(current)) {
+                    newobj[attr] = current;
+                } else {
+                    newobj[attr] = [current];
+                }
+            }
+        };
+    }
+    return newobj;
+};
+
+Gable.data.types.raw.transform.validate = function(obj) {
+
+};
+
+
+/* type utilities */
+
+/* gets a type; generic */
+/* returns type object else null if undefined */
+Gable.data.type.get = function(type) {
+    var types = Gable.data.types[type];
+    return ('undefined' !== typeof types) ? types : null;
+};
+
+/* sets a type; generic */
+/* returns false if doesn't validate, else true */
+Gable.data.type.set = function(type, obj) {
+    //TODO: if has a validate method, use it
+    if (Gable.data.type.validate(type, obj)) {
+        Gable.data.types[type] = obj;
+        return true;
+    }
+    return false;
+};
+
+/* updates invidiual attributes of a type; generic */
+/* returns value of Gable.data.type.set */
+Gable.data.type.update = function(type, attrs) {
+    var typeobj = Gable.data.type.get(type);
+    for (var attr in attrs) {
+        typeobj[attr] = attrs[attr];
+    }
+    return Gable.data.type.set(type, typeobj);
+};
+
+/* type transformsFrom other_type if other_type transformsTo type */
+/* returns inverse of Gable.data.type.tranformsTo */
+Gable.data.type.tranformsFrom = function(type, other_type) {
+    //TODO: real implementation
+    return Gable.data.type.tranformsTo(other_type, type);
+};
+
+/* returns true if a type can be converted from another type, else false */
+Gable.data.type.tranformsTo = function(type, other_type) {
+    if ('undefined' !== typeof Gable.data.types[type] && 'undefined' !== typeof Gable.data.types[type]['transform'] && 'function' === typeof Gable.data.types[type]['transform'][other_type]) {
+        return true;
+    }
+    return false;
+};
+
+/* returns  null if can't be transformed from type to other_type, else returns a transformed object of type to other_type */
+Gable.data.type.transform = function(type, other_type, obj) {
+    if (Gable.data.type.tranformsTo(type, other_type)) {
+        return Gable.data.types[type]['transform'][other_type](obj);
+    }
+    return null;
+};
+
+/* returns true if a type can be validated, else false. note that this is different than testing whether the type object can be validated (which is always true). */
+Gable.data.type.canValidate = function(type) {
+    var typeobj = Gable.data.type.get(type);
+    if ('function' === typeof typeobj.validate) {
+        return true;
+    }
+    return false;
+};
+
+
+/* validates a type object (note that this is different than
+    validating an object for a type i.e. Gable.data.value.validate()) */
+Gable.data.type.validate = function(type, obj) {
+
+    // Check to see if confirms to a vaild type
+    // using a set of tests. returns false if a test fails.
+    // Well-formed (if present) methods:
+    //   obj.validate: fn 
+    //   obj.transform: { 'optional1': fn }
+    //   obj.filter: fn
+    // * Denotes required
+    if ('undefined' === typeof obj) {
+        return false;
+    }
+    if ('undefined' !== typeof obj.validate && 'function' !== typeof obj.validate) {
+        return false;
+    }
+    if ('undefined' !== typeof obj.filter && 'function' !== typeof obj.filter) {
+        return false;
+    }
+    if ('undefined' !== typeof obj.transform) {
+        for (var attr in obj.transform) {
+            if ('undefined' !== typeof obj.transform[attr] && 'function' !== typeof obj.transform[attr]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+
+};
+
+/* value utilities */
+
+/* values are composite data types made up of a type and value property. a value can also have an optional timestamp property. */
+
+/* returns true if an object's own types or one layer connections can convert to a type, else false */
+Gable.data.value.transformsTo = function(obj, other_type) {
+    var type = Gable.data.value.type(obj);
+    if (Gable.data.value.transformsTo(type, other_type)) {
+        return true;
+    }
+    return false;
+};
+
+/* returns true if an object's own types or one layer connections can convert to a type, else false */
+Gable.data.value.transformsFrom = function(obj, other_type) {
+    var type = Gable.data.value.type(obj);
+    if (Gable.data.value.transformsFrom(type, other_type)) {
+        return true;
+    }
+    return false;
+};
+
+/* returns an object converted to type */
+Gable.data.value.transform = function(obj, other_type) {
+    var type = Gable.data.value.type(obj);
+    return Gable.data.type.transform(type, other_type, obj);
+};
+
+/* returns type_id for an object or null if not set */
+Gable.data.value.type = function(obj) {
+    //TODO: Implementation; also: how?
+    return ('undefined' !== typeof obj.type) ? obj.type : null;
+};
+
+/* returns true if value is valid instance of type, else false */
+Gable.data.value.isType = function(obj, type) {
+    if (type === Gable.data.value.type(obj)) {
+        return true;
+    }
+    return false;
+};
+
+/* returns true if an object can be validated for a type, else false. note that this is different than testing whether the type object can be validated (which is always true). */
+Gable.data.value.canValidate = function(obj) {
+    var type = Gable.data.value.type(obj);
+    if (Gable.data.type.canValidate(type)) {
+        return Gable.data.value.validate(obj, type);
+    }
+};
+
+/* returns true if object can and does validate for a type, else false */
+Gable.data.value.validate = function(obj, type) {
+    if (Gable.data.value.canValidate(type)) {
+        var typeobj = Gable.data.type.get(type);
+        if (typeobj.validate(obj)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+
+/* Element Types */
+
+Gable.data.elements = {
+    'table': {
+        'validate': function() {}
+    },
+    'column': {
+        'validate': function() {}
+    },
+    'row': {
+        'validate': function() {}
+    },
+    'cell': {
+        'validate': function() {}
+    }
+};
+
+/* Chart Types */
+
+Gable.charts.types = {
+    'bar': {
+        'update*': function(target, table) {},
+        'remove': function(target, table) {},
+
+        'replace': function(target, table) {},
+        'add': function(target, table) {}
+    },
+    'pie': {
+        'update*': function(target, table) {},
+        'remove': function(target, table) {},
+        'replace': function(target, table) {},
+        'add': function(target, table) {}
+    },
+    'line': {
+        'update*': function(target, table) {},
+        'remove': function(target, table) {},
+        'replace': function(target, table) {},
+        'add': function(target, table) {}
+    }
+};
+
+
+/* Chart utilities */
+
+Gable.charts.add = function(type, target) {
+
+};
+
+Gable.charts.type = function(target) {
+
+};
+
+Gable.charts.exists = function(id) {
+
+};
+
+Gable.charts.remove = function(id) {
+
+};
+
+//Create
+
+/* returns a well formatted data object for data type or null if there was some sort of error. columns and rows are arrays of column and row objects, respectively.  */
+Gable.data.table.create = function( columns, rows, meta, timestamp ) {
+	//columns are rows are required
+	if( 'undefined' === typeof columns || 'undefined' === typeof rows ||  null === columns || null === rows ) {
+		return null;	
+	} else {
+		//columns and rows are formatted as arrays in raw
+		if( !Gable.utils.isArray( columns ) || !Gable.utils.isArray( rows ) {
+			//columns and rows must be arrays
+			return null;
+		}
+	}
+	//meta is optional
+	if( 'undefined' === typeof meta ) {
+		meta = null;
+	}
+	//timestamp can be faked
+	timestamp = ( 'number' !== typeof timestamp ) ? new Date().getTime() : timestamp;
+	return { 
+		'columns': columns
+		, 'rows': rows
+		, 'meta': meta
+		, 'timestamp': timestamp
+	};
+};
+Gable.data.row.create = function( value, meta, timestamp ) {
+	//value is required
+	if( 'undefined' === typeof value || null === value ) {
+		return null;	
+	}
+	//meta is optional
+	if( 'undefined' === typeof meta ) {
+		meta = null;
+	}
+	//timestamp can be faked
+	timestamp = ( 'number' !== typeof timestamp ) ? new Date().getTime() : timestamp;
+    	return { 
+        	'value': value
+		, 'meta': meta
+       		, 'timestamp': timestamp
+	};
+};
+
+/* type is required */
+/* types: boolean, number, string, date, datetime */
+Gable.data.column.types = [ 'boolean', 'number', 'string', 'date', 'datetime' ];
+Gable.data.column.create = function( type, id, meta, timestamp ) {
+	//type is required
+	if( 'undefined' === typeof type ||  null === type ) {
+		//no type given
+		return null;
+	} else {
+		if( -1 === Gable.data.column.types.indexOf( type ) ) {
+			//not a valid type
+			return null;
+		}
+	}
+	//id is optional
+	if( 'undefined' === typeof id || null === id ) {
+		id = null;
+	}
+	//meta is optional
+	if( 'undefined' === typeof meta || null === meta ) {
+		meta = {};
+	}
+	if( 'undefined' === typeof meta.label ) {
+		meta.label = null;
+	}
+	//timestamp can be faked
+	timestamp = ( 'number' !== typeof timestamp ) ? new Date().getTime() : timestamp;
+	return { 
+		'type': type
+		, 'id': id
+    		, 'meta': meta
+		, 'timestamp': timestamp
+	};
+};
+
+Gable.data.cell.create = function( value, meta, id, timestamp) {
+	//value is required
+	if( 'undefined' === typeof value || null === value ) {
+		//no value given
+		return null;
+	}
+	//id is optional
+	if( 'undefined' === typeof id || null === id ) {
+		id = null;
+	}
+	//meta is optional
+	if( 'undefined' === typeof meta || null === meta ) {
+		meta = {};
+	}
+	if( 'undefined' === typeof meta.label ) {
+		meta.label = null;
+	}
+
+	//timestamp can be faked
+	timestamp = ( 'number' !== typeof timestamp ) ? new Date().getTime() : timestamp;
+
+	return { 
+		'value': value
+		, 'id': id
+    		, 'meta': meta
+		, 'timestamp': timestamp
+	};
+};
+    
+Gable.data.table.add = function( table_id, table ) {
+	//TODO: validate table
+	if( 'undefined' === typeof Gable.cache[ table_id ] ) {
+		Gable.cache[ table_id ] = table;
+	} 
+};
+
+Gable.data.row.add = function( table_id, row ) {
+	//TODO: validate row 
+	var table = Gable.cache[ table_id ];
+	if( 'undefined' !== typeof table ) {
+		Gable.cache[ table_id ].rows.push( row );
+	};
+};
+
+Gable.data.column.add = function( table_id, column ) {
+	//TODO: validate column 
+	var table = Gable.cache[ table_id ];
+	if( 'undefined' !== typeof table ) {
+		Gable.cache[ table_id ].columns.push( columns );
+	};
+};
+
+//Can't update a cell without adding a row or column
+//Gable.data.cell.add = function() {};
+
+//Read
+Gable.data.table.get = function( table_id ) {
+	if( 'undefined' !== typeof Gable.cache[ table_id ] ) {
+		return Gable.cache[ table_id ];
+	} 
+	return null;
+};
+
+//allows for optional lookup by row_id, in which case row_index is ignored and can be set to null
+Gable.data.row.get = function( table_id, row_index, row_id ) {
+	var table = Gable.cache[ table_id ]
+	  , row;
+	if( 'undefined' !== typeof table ) {
+		if( 'undefined' !== typeof row_id ) {
+			var rowlen = table.rows.length;
+			for( var x = 0; x < rowlen; x += 1 ) {
+				var tmp = table.rows[ x ];
+				if( 'undefined' !== tmp.id ) {
+					if( tmp.id === row_id ) {
+						return;
+					}
+				}
+			}
+		} else {
+			row = table.rows[ row_index ];
+			if( 'undefined' !== typeof row ) {
+				return row;
+			}
+		}
+	} 
+	return null;
+};
+Gable.data.column.get = function( table_id, column_index, column_id) {
+	var table = Gable.cache[ table_id ]
+	  , column;
+	if( 'undefined' !== typeof table ) {
+		if( 'undefined' !== typeof column_id ) {
+			var collen = table.rows.length;
+			for( var x = 0; x < collen; x += 1 ) {
+				column = table.columns[ x ];
+				if( 'undefined' !== columm.id ) {
+					if( column.id === column_id ) {
+						return column;
+					}
+				}
+			}
+		} else {
+			column = table.columns[ column_index ];
+			if( 'undefined' !== typeof column ) {
+				return column;
+			}
+		}
+	} 
+	return null;
+};
+
+Gable.data.cell.get = function( table_id, row_index, row_id, column_index, column_id ) {
+	var table = Gable.cache[ table_id ]
+	  , column, row, value;
+	if( 'undefined' !== typeof table ) {
+		if( 'undefined' !== typeof row_id ) {
+			var rowlen = table.rows.length;
+			for( var x = 0; x < rowlen; x += 1 ) {
+				var tmp = table.rows[ x ];
+				if( 'undefined' !== tmp.id ) {
+					if( tmp.id === row_id ) {
+						row = tmp;
+					}
+				}
+			}
+		} else {
+			row = table.rows[ row_index ];
+		}
+	} 
+	if( 'undefined' !== typeof row ) {
+		if( 'undefined' !== typeof column_id ) {
+			var rowlen = table.rows.length;
+			for( var x = 0; x < rowlen; x += 1 ) {
+				var tmp = table.rows[ x ];
+				if( 'undefined' !== tmp.id ) {
+					if( tmp.id === column_id ) {
+						value = tmp;
+					}
+				}
+			}
+		} else {
+			value = row[ column_index ];
+		}
+	}
+	return null;
+};
+//TODO: Next up?
+//Destroy
+Gable.data.remove = function() {};
+Gable.data.table.remove = function() {};
+Gable.data.row.remove = function() {};
+Gable.data.column.remove = function() {};
+Gable.data.cell.remove = function() {};
+
+//Replace
+Gable.data.put = function() {};
+Gable.data.table.put = function() {};
+Gable.data.row.put = function() {};
+Gable.data.column.put = function() {};
+Gable.data.cell.put = function() {};
+
+//Update
+Gable.data.update = function(data, id, type, format, meta,parents,children) {
+
+};
+
+Gable.data.table.update = function() {};
+Gable.data.row.update = function() {};
+Gable.data.column.update = function() {};
+Gable.data.cell.update = function() {};
+
+Gable.data.update.value = function() {};
+Gable.data.table.update.value = function() {};
+Gable.data.row.update.value = function() {};
+Gable.data.column.update.value = function() {};
+Gable.data.cell.update.value = function() {};
+
+Gable.data.update.id = function() {};
+Gable.data.table.update.id = function() {};
+Gable.data.row.update.id = function() {};
+Gable.data.column.update.id = function() {};
+Gable.data.cell.update.id = function() {};
+
+Gable.data.update.meta = function() {};
+Gable.data.table.update.meta = function() {};
+Gable.data.row.update.meta = function() {};
+Gable.data.column.update.meta = function() {};
+Gable.data.cell.update.meta = function() {};
+
+Gable.data.update.timestamp = function() {};
+Gable.data.table.update.timestamp = function() {};
+Gable.data.row.update.timestamp = function() {};
+Gable.data.column.update.timestamp = function() {};
+Gable.data.cell.update.timestamp = function() {};
+
+//Draw
+Gable.data.draw = function() {};
+Gable.data.table.draw = function() {};
+Gable.data.row.draw = function() {};
+Gable.data.column.draw = function() {};
+Gable.data.cell.draw = function() {};
+
+/* Utilities */
+
+Gable.utils.isArray = function(obj) {
+    if (!obj.isArray) {
+        return Object.prototype.toString.call(arg) == '[object Array]';
+    } else {
+        return obj.isArray();
+    }
+}
+
