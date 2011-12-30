21d2ad8e67c93ed4869e78b14c939a90c25f665e
Taylor Buley <taylor.buley@gmail.com>
2011-12-30 01:49:12 -0500

    ...

--- gable.js
+++ gable.js
@@ -177,17 +177,15 @@ Gable.data.types.input.transform.iterateObjectColumns = function(value) {
 			if (Gable.utils.isArray(value[attr])) {
 				var valuelen = value[ attr ].length;
 				var nonulls = true;
-				if ( null !==  value[ attr ][ x ] ) {
-					for( var x = 0; x < valuelen; x += 1 ) {
-						if( null !== value[ attr ][ x ] && 'object' === typeof value[ attr ][ x ]  && !Gable.utils.isArray( value[ attr ][ x ] ) && !( value[ attr ][ x ] instanceof Date ) ) { 
-							for( var attr2 in value[ attr ][ x ] ) {
-								if( null === value[ attr ][ x ][ attr2 ] ) {
-									nonulls = false;
-								}
+				for( var x = 0; x < valuelen; x += 1 ) {
+					if( null !== value[ attr ][ x ] && 'object' === typeof value[ attr ][ x ]  && !Gable.utils.isArray( value[ attr ][ x ] ) && !( value[ attr ][ x ] instanceof Date ) ) { 
+						for( var attr2 in value[ attr ][ x ] ) {
+							if( null === value[ attr ][ x ][ attr2 ] ) {
+								nonulls = false;
 							}
-						} else if( null === value[ attr ][ x ] ) {
-							nonulls = false;
 						}
+					} else if( null === value[ attr ][ x ] ) {
+						nonulls = false;
 					}
 				}
 				if( null === biggest || ( true === nonulls && valuelen >= biggest_total ) ) {
