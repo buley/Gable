9eb0b5c3b03007276c0a98435ef92b5a9be65995
Taylor Buley <taylor.buley@gmail.com>
2012-01-01 01:34:49 -0500

    update

--- gable.js
+++ gable.js
@@ -35,7 +35,7 @@ var Gable = (function(){
 			}
 			return;
 		}
-		Private.data.add( current_table, req.value );
+		Private.data.add( current_table, req.value, req.meta );
 		if( 'function' === typeof req.on_success ) {
 			req.on_success( req.id );	
 		}
