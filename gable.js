fb41b7616da0f27aae4daab30b815e88cd1c882c
Taylor Buley <taylor.buley@gmail.com>
2012-01-02 20:55:33 -0500

    update

--- gable.js
+++ gable.js
@@ -1716,6 +1716,7 @@ var Gable = (function(){
 
 	Private.data.column.remove = function( val, table_id, column, column_id, column_meta, on_success, on_error ) {
 		//TODO: validate column 
+
 		var table = Private.cache[ table_id ];
 		var col = table.columns[ column ];
 		if( 'undefined' !== typeof col ) {
@@ -1737,15 +1738,21 @@ var Gable = (function(){
 	Private.data.cell.remove = function( table_id, row, column, on_success, on_error ) {
 	
 		var table = Private.cache[ table_id ];
+
 		if( 'undefined' !== typeof table.rows[ row ].value[ column ] ) {
+
 			delete table.rows[ row ].value[ column ];
+
 			if( 'function' === typeof on_success ) {
 				on_success( { 'table': table_id, 'row': row, 'column': column }  );	
 			}
 
 		} if( 'function' === typeof on_error ) {
+
 			on_error( { 'table': table_id, 'row': row, 'column': column } );	
+
 		}
+
 	};
 
 
