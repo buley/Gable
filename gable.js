5f62e48b793bfe24b82beda8a1771abacb669fa0
Taylor Buley <taylor.buley@gmail.com>
2012-01-04 00:02:01 -0500

    update

--- gable.js
+++ gable.js
@@ -330,12 +330,26 @@ var Gable = (function(){
 			}
 			var x = 0, idlen = find_result, finditem;
 			for( x = 0; x < idlen; x += 1 ) {
+
 				find_item = find_result[ x ];
 				var row = find_item.row;
 				var column = find_item.column;
 				var value = req.value;
+				var attr;
 				var meta = req.meta;
 				var id = req.id;
+
+				if( 'undefined' !== typeof id ) {
+					find_item.id = id;
+				}
+
+				if( 'undefined' !== typeof meta ) {
+					find_item.meta = meta;
+				}
+
+				if( 'undefined' !== typeof meta ) {
+					find_item.value = value;
+				}
 
 				var find_update_on_success = function( res ) {
 					Private.charts.redraw( table_id );
@@ -360,14 +374,14 @@ var Gable = (function(){
 				}
 
 				if( 'undefined' !== typeof row && null !== row && 'undefined' !== typeof column && null !== column ) {
-					Private.data.cell.update( value, table_id, row, column, find_update_on_success, find_update_on_error );
+					Private.data.cell.update( find_item, table_id, row, column, find_update_on_success, find_update_on_error );
 				} else if( 'undefined' !== typeof row && null !== row ) {
-					Private.data.row.update( value, table_id, row, id, meta, find_update_on_success, find_update_on_error );
+					Private.data.row.update( find_item, table_id, row, id, meta, find_update_on_success, find_update_on_error );
 				} else if( 'undefined' !== typeof column && null !== column ) {
 				
-					Private.data.column.update( value, table_id, column, id, meta, find_update_on_success, find_update_on_error );
+					Private.data.column.update( find_item, table_id, column, id, meta, find_update_on_success, find_update_on_error );
 				} else {
-					Private.data.table.update( value, table_id, id, meta, find_update_on_success, find_update_on_error );
+					Private.data.table.update( find_item, table_id, id, meta, find_update_on_success, find_update_on_error );
 				}
 
 			}
