e3f48b3b2ddcd82033d191d91a23aee830080047
Taylor Buley <taylor.buley@gmail.com>
2012-01-05 00:54:27 -0500

    update

--- gable.js
+++ gable.js
@@ -401,8 +401,9 @@ var Gable = (function(){
 		Find.prototype = Public.prototype;
 		Find.prototype.dump = function() {
 			var req = arguments[ 0 ];
-			var find_item, findlen = find_items.length, x = 0;
+			var find_item, findlen = find_result.length, x = 0;
 			for( var x = 0; x < findlen; x += 1 ) {
+				find_item = find_result[ x ];
 				if( 'undefined' === typeof req ) {
 					Public.prototype.dump( null, find_item );
 				} else {
